1c1e126eccaa8553de53d0ef4e46defa8fa2b576
Dasharks <159520477+Dasharks@users.noreply.github.com>
2024-02-09 20:25:28 +0530

    Delete root directory

--- root/provider.tf
+++
@@ -1,8 +0,0 @@
-# --------------------------
-# AWS Provider Configuration
-# --------------------------
-
-# Configure the AWS provider with the specified region.
-provider "aws" {
-  region = var.region
-}
